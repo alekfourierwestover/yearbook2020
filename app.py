ab9448344fc3b7cd164a21a21f66b312d4110991
Xingbai He <JoyHe@Xingbais-Air.fios-router.home>
2020-04-15 19:56:38 -0400

    joy's stuff

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-def registration():
-    pass
+from flask import Flask, render_template, url_for, request, redirect
+
+app = Flask(__name__)
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/sendmessage", methods=["POST", "GET"])
 def send_message():
-    pass
+    if request.method == "POST":
+        message = request.form["message"]
+        sendTo = request.form["sendTo"]
+        return redirect(url_for("sentmessages", usr=message))
+    else:
+        return render_template('sendmessage.html')
+
+@app.route("/<usr>")
+def sentmessages(usr):
+    return f"<p>{usr}</p>"
+
+if __name__=="__main__":
+    app.run(debug = True)
