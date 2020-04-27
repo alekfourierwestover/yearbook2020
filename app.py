b6c5b81d47dd18de0fe80d7b2c0a2fa44ac4d18d
Ziyong <ziyongcuii@gmail.com>
2020-04-27 15:47:30 +0000

    important changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 from passlib.hash import sha256_crypt
 import uuid
 import os
-# session is a dictionary that is stored client side as a cookie
+import random
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRETKEY")
@@ -22,17 +22,18 @@ mail = Mail(app)
 def safestr(bad_txt):
     return str(uuid.uuid5(uuid.NAMESPACE_URL, bad_txt))
 
-@app.route('/send-mail/', methods=("GET",))
-def send_mail():
+@app.route('/send_verification_code/', methods=("GET",))
+def session_send_verification_code():
     try:
-        msg = Message("server sending email test", sender=app.config.get("MAIL_USERNAME"), recipients=["alek.westover@gmail.com"])
-        msg.body = "Hey Alek, I think that you should look into getting the server to send emails. Thanks, bhsyearbook.tech"
+        msg = Message("Verification Code", sender=app.config.get("MAIL_USERNAME"), recipients=[session["email"]])
+        msg.body = f"Hey {session['username'], \nThank you for creating an account on the bhsyearbook.tech website. You're verification code is {session['verification_code']}. Please use this at <a href='bhsyearbook.tech/verify'>bhsyearbook.tech/verify</a>"
         mail.send(msg)
         return 'Mail sent!'
     except Exception as e:
         return str(e)
 
-# seems to do the trick!!! just cant login multiple users on one device, which is good anyways!!!!
+# session seems to do the trick!!! just cant login multiple users on one device, which is good anyways!!!!
+# session is a dictionary that is stored client side as a cookie
 @app.route("/get_username", methods=("GET",))
 def get_username():
     return session.get("username")
@@ -46,42 +47,60 @@ def get_uuid():
 @app.route("/")
 def serve_index():
     if session.get("loggedin"):
-        return redirect(url_for("serve_main"))
+        if session.get("verified"):
+            return redirect(url_for("serve_main"))
+        else:
+            return redirect(url_for("serve_verify"))
     else:
         return render_template("index.html")
 
 @app.route("/main")
 def serve_main():
     if session.get("loggedin"):
-        return render_template("main.html")
+        if session.get("verified"):
+            return redirect(url_for("serve_main"))
+        else:
+            return redirect(url_for("serve_verify"))
     else:
         return redirect(url_for("serve_index"))
 
 @app.route("/mymessages")
 def serve_mymessages():
     if session.get("loggedin"):
-        return render_template("mymessages.html")
+        if session.get("verified"):
+            return render_template("mymessages.html")
+        else:
+            return redirect(url_for("serve_verify"))
     else:
         return redirect(url_for("serve_index"))
 
 @app.route("/sendmessages")
 def serve_sendmessages():
     if session.get("loggedin"):
-        return render_template("sendmessages.html")
+        if session.get("verified"):
+            return render_template("sendmessages.html")
+        else:
+            return redirect(url_for("serve_verify"))
     else:
         return redirect(url_for("serve_index"))
 
 @app.route("/edit")
 def serve_edit():
     if session.get("loggedin"):
-        return render_template("edit.html")
+        if session.get("verified"):
+            return render_template("edit.html")
+        else:
+            return redirect(url_for("serve_verify"))
     else:
         return redirect(url_for("serve_index"))
 
 @app.route("/map")
 def serve_map():
     if session.get("loggedin"):
-        return render_template("map.html")
+        if session.get("verified"):
+            return render_template("map.html")
+        else:
+            return redirect(url_for("serve_verify"))
     else:
         return redirect(url_for("serve_index"))
 
@@ -89,6 +108,7 @@ def serve_map():
 @app.route("/logout", methods=("POST","GET"))
 def handle_logout():
     session["loggedin"] = False
+    session["verified"] = False
     session["username"] = ""
     session["uuid"] = ""
     return redirect(url_for("serve_index"))
@@ -112,6 +132,8 @@ def handle_login():
                 session["username"] = user_name
                 session["uuid"] = safe_user_name
                 session["loggedin"] = True
+                session["verified"] = data[safe_user_name]["verified"]
+                session["verification_code"] = data[safe_user_name]["verification_code"]
                 return redirect(url_for("serve_main"))
             else:
                 return redirect(url_for("serve_index", error="password_wrong"))
@@ -124,19 +146,19 @@ def handle_register():
     with open("users.json", "r") as f:
         x = json.load(f)
         user_name = request.form["name"].lower()
-
         
         tmp = user_name.split(" ")
         for leon in range(len(tmp)):
             tmp[leon] = tmp[leon][0].upper() + tmp[leon][1:]
         user_name = " ".join(tmp)
-        
-
 
         safe_user_name = safestr(user_name)
         session["username"] = user_name
         session["uuid"] = safe_user_name
         session["loggedin"] = True
+        session["verified"] = False
+        session["email"] = request.form["email"]
+        session["verification_code"] = safestr(str(random.random()))
 
         try:
             img_stream = request.files.get("profilepic").stream
@@ -150,12 +172,15 @@ def handle_register():
             "email": request.form["email"],
             "password": sha256_crypt.hash(request.form["password"]),
             "institution": request.form["institution"],
-            "bio": request.form["bio"]
+            "bio": request.form["bio"],
+            "verified": False,
+            "verification_code": session["verification_code"]
         }
         with open("users.json", "w") as f:
             json.dump(x, f, indent = 4)
 
-        return redirect(url_for("serve_main"))
+        session_send_verification_code()
+        return redirect(url_for("serve_verify"))
 
 @app.route("/getProfiles", methods=("GET",))
 def getProfiles():
@@ -178,8 +203,6 @@ def handle_send_message():
 
     if send_to not in user_data.keys():
         session["loggedin"] = False
-        session["username"] = ""
-        session["uuid"] = ""
         return url_for("serve_index", error="malicious_user")
 
     if not send_to in message_data.keys():
