7adb6cc5f2926af9ed7890f0c9fc62d5e5cc2557
Alek Westover <alek.westover@gmail.com>
2020-05-06 00:07:06 -0400

    init project script run with sh initialize_project.sh

---
+++ initialize_project.sh
@@ -0,0 +1,17 @@
+cd static 
+npm i
+cd ..
+mkdir data
+cd data
+
+mkdir belmonthigh
+cd belmonthigh
+echo "{}" > users.json
+echo "{}" > messages.json
+echo "{}" > verification_codes.json
+echo "{}" > passwords.json
+echo "{}" > request.json
+
+echo "[\"belmonthigh\"] "
+echo "{ \"belmonthigh\": { \"senior\": [\"20@belmontschools.net\"], \"all\": [\"@belmontschools.net\", \"@belmont.k12.ma.us\"] } }
+
